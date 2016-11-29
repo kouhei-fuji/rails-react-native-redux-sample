6133ab09bb061227215e3d9892b7855029da48cb
Kouhei Fujigaya <kouhei.fujigaya@gmail.com>
2016-11-29 19:02:01 +0900

    Bugfix: if state is changed, re-render

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,13 @@
-import React, { Component, PropTypes } from 'react'
-import { bindActionCreators } from 'redux'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Router } from 'react-native-router-flux'
 
-import * as actions from '../actions'
 import scenes from '../scenes'
 
 const RouterWithRedux = connect()(Router)
 
-class App extends Component {
-  static propTypes = {
-    actions: PropTypes.object.isRequired,
-    state:   PropTypes.object.isRequired,
-  }
-
+export default class App extends Component { // [MEMO] Move to `src/index.js`?
   render() {
-    const { actions, state } = this.props
-    return <RouterWithRedux scenes={scenes} actions={actions} state={state} />
+    return <RouterWithRedux scenes={scenes} />
   }
 }
-
-function mapStateToProps(state) {
-  return { state }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(actions, dispatch)
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
--- src/scenes.js
+++ src/scenes.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { Scene, Actions } from 'react-native-router-flux'
+
+import * as actions from './actions'
 
 import Posts from './containers/Posts'
 
+function mapStateToProps(state) {
+  return { state }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+function connected(Container) {
+  return connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Container)
+}
+
 export default Actions.create(
   <Scene key="app">
-    <Scene key="posts" component={Posts} hideNavBar initial />
+    <Scene key="posts" component={connected(Posts)} hideNavBar initial />
   </Scene>
 )
